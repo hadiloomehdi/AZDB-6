7221a720e68cdb962ff40653ff1ee0e37bf86fc2
mahdi hadiloo <hadiloomehdi@gmail.com>
2020-12-18 14:58:24 +0330

    add genre user and books

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
+import { UserModule } from './user/user.module';
+import { GenreModule } from './genre/genre.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [HelloModule],
+  imports: [HelloModule, UserModule, GenreModule, BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
